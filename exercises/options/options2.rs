16a1107b0efa276b03e805bf819ad51e6a925dd2
Glenn Jenkins (Chromebook) <gljenkins@cardiffmet.ac.uk>
2023-05-11 17:28:44 +0100

    Finshed working on options2, working with options is quite tricky.

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,8 +1,6 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -10,8 +8,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        // TODO: Make this an if let statement whose value is "Some" 
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -26,8 +24,9 @@ mod tests {
 
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`'s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, range);
+        while let Some(integer) = optional_integers.pop() {
+            assert_eq!(integer.unwrap(), range); // have to either unwrap the left
+            assert_eq!(integer, Some(range)); // or make the right an option so types are the same.
             range -= 1;
         }
     }
