4fa1770b06a9566a2949aa92f112d063be439648
Glenn Jenkins (Chromebook) <gljenkins@cardiffmet.ac.uk>
2023-05-11 17:35:39 +0100

    Completed options3, looks like when you do a match the resulting thing from the option can't be copied.

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,7 +1,6 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -12,7 +11,9 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // does some kind of partial copy here, failes unless this is a reference. 
+        // can't copy as the point type doesn't implement copy.
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
