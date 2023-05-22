274e25a009c9600ba8ef03909f5f1d35a26623cd
gljenkins@cardiffmet.ac.uk <gljenkins@cardiffmet.ac.uk>
2023-05-22 17:29:16 +0100

    Few more rust exercises done while I had a few minuets.

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -25,12 +24,19 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+
+    // The ? says, use the value if it's Ok else treat as an Err and do the 
+    // map_err - this is weird. 
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parse_int)?;
+
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
